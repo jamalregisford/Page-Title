3eddda6d08b4ab377a22b1681670a8bb0bcbf279
jamalregisford <32132171+jamalregisford@users.noreply.github.com>
2018-12-06 09:57:32 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#bro2").click(function(){
+    window.location.href = "https://www.youtube.com/";
+    });
+    
+    $("#bro3").click(function(){
+    window.location.href = "  https://support.google.com/youtube/answer/1646861?hl=en";
+    });
+  
+  $("#bro1").click(function(){
+    window.location.href = "https://www.youtube.com/feed/trending";
+    });
